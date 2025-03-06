5cf611f776f73b09cba168d593c2ca006eedb0b5
Arman Bhatia <arman.bhatia.1407@gmail.com>
2025-03-06 14:48:12 -0500

    added four buttons on the main

---
+++ lib/view/expenseTracker/ExpenseTracker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExpensetrackerHome extends StatefulWidget {
+  const ExpensetrackerHome({super.key});
+
+  @override
+  State<ExpensetrackerHome> createState() => _ExpensetrackerHomeState();
+}
+
+class _ExpensetrackerHomeState extends State<ExpensetrackerHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
